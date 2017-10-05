94edcb4103b830ab3b5c560f554e8f8eb7b63b2d
Troy Leyda <tleyda@pillartechnology.com>
2017-10-05 18:29:03 -0500

    Handle bad json in post body

--- app_test.go
+++ app_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 )
 
 func TestHandler__returns_correct_responses(t *testing.T) {
@@ -68,6 +69,23 @@ func TestGetResponse__returns_not_a_jam_for_1(t *testing.T) {
 
 func TestPostHandler__returns_bad_request_with_no_body(t *testing.T) {
 	req, err := http.NewRequest("POST", "/jams", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	test_handler := http.HandlerFunc(jamPostHandler)
+
+	test_handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Expected %v, got %v", http.StatusBadRequest, status)
+	}
+}
+
+func TestPostHandler__returns_bad_request_with_bad_json(t *testing.T) {
+	reader := strings.NewReader("this is not json")
+	req, err := http.NewRequest("POST", "/jams", reader)
 	if err != nil {
 		t.Fatal(err)
 	}
