cedd88242760980e9cd7585eca66c6587bbc2e77
Troy Leyda <troy.leyda@gmail.com>
2017-10-22 01:22:11 -0500

    Move fake data store to its own file

--- jamstore_test.go
+++ jamstore_test.go
@@ -7,29 +7,6 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
-type FakeDataStore struct {
-	StoredJam Jam
-	Error error
-}
-
-func (fake *FakeDataStore)Put(jam Jam) error {
-	fake.StoredJam = jam
-	return nil
-}
-
-func (fake *FakeDataStore)Get(jamText string) (Jam, error) {
-	jam := Jam{"", false}
-	err := fake.Error
-	if err == nil {
-		if jamText == fake.StoredJam.JamText {
-			jam = fake.StoredJam
-		} else {
-			err = datastore.ErrNoSuchEntity
-		}
-	}
-	return jam, err
-}
-
 func TestStoreJam__correctly_stores_jam_in_datastore(t *testing.T) {
 	fakeDataStore := new(FakeDataStore)
 
